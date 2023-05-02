f6d4ebe5128005d2d07b97658a0b0c580a243344
Иван Книженцев <prog63@iMac-User.local>
2023-05-02 16:36:57 +0500

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,39 +1,24 @@
-require 'faker'
+require "factory_bot_rails"
 
-data = [
-  {
-    user: { email: "test@mail.ru", password: "test_password" },
-    posts: [
-      { title: "test_title", text: "test_text" }
-    ],
-    comments: {}
-  },
-  {
-    user: {},
-  },
-]
-
-if Rails.env.development?
+def create_admin_user
   AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 end
 
 def create_test_user
   user = User.create!(email: "test@mail.ru", password: "test_password")
-  Post.insert_all(test_user_posts)
+  posts = FactoryBot.create_list(:post, rand(2..10), user: user)
+  FactoryBot.create_list(:comment, rand(2..5), post: posts.sample)
 end
 
-def create_other_users
-
-end
-
-def create_data
-  data_for_user.each do |user|
-    user = User.create!(**user)
+def create_other_users_and_posts
+  FactoryBot.create_list(:user, rand(2..10)).each do |user|
+    posts = FactoryBot.create_list(:post, rand(2..10), user: user)
+    FactoryBot.create_list(:comment, rand(2..5), post: posts.sample)
   end
 end
 
-# unless Rails.env.production?
-#   create_test_user
-#   create_other_users
-#   create_data
-# end
+unless Rails.env.production?
+  create_test_user
+  create_other_users_and_posts
+  create_admin_user
+end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -4,6 +4,7 @@ require "rails_helper"
 
 RSpec.describe PostsController, type: :controller do
   let(:user) { create(:user) }
+  let(:user_post) { create(:post, user: user) }
 	let(:post) { create(:post) }
 
   before do
@@ -33,5 +34,57 @@ RSpec.describe PostsController, type: :controller do
       get :show, params: { id: post.id }
       expect(response.status).to eq(200)
     end
+
+    it "создаст просмотр, если current_user не автор поста" do
+      expect { get :show, params: { id: post.id } }.to change { View.exists?(post_id: post.id) }.from(false).to(true)
+    end
+
+    it "не создаст просмотр, если current_user автор поста" do
+      get :show, params: { id: user_post.id } 
+      expect(View.exists?(post_id: user_post.id)).to be_falsey
+    end
+  end
+
+  describe "#edit" do
+    it "если передан не верный id юзера вернет 404" do
+      expect { get :edit, params: { id: rand(1_000..2_000) } }.to raise_error(ActionController::RoutingError)
+    end
+
+    it "если верно передан id юзера вернет статус 200" do
+      get :edit, params: { id: post.id }
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe "#create" do
+    context "положительный сценарий" do
+      it "при верных данных вернет 200" do
+        post :create, params: success_data
+        expect(response.status).to eq(200)
+      end
+
+      it "создаст пост" do
+        post :create, params: success_data
+        expect(Post.where(user: user, **success_data)).to be_present
+      end
+    end
+
+    context "негативный сценарий" do
+      it "при верных данных вернет 422" do
+        post :create, params: { title: "", text: "" }
+        expect(response.status).to eq(422)
+      end
+
+      it "не создаст пост" do
+        expect { post :create, params: { title: "", text: "" } }.to change { Post.where(user: user, **success_data).present? }.from(false).to(false)
+      end
+    end
+  end
+
+  def success_data
+    {
+      title: "test_title",
+      text: "text_title"
+    }
   end
 end
